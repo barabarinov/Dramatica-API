ef2ad68b8108c2648c4997b7e3a1ffd5b5c7026e
barabarinov <barabarinov@gmail.com>
2024-03-28 10:56:08 +0200

    added permission settings to REST_FRAMEWORK

--- config/settings.py
+++ config/settings.py
@@ -139,6 +139,9 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
+    "DEFAULT_PERMISSION_CLASSES": [
+        "theatre.permissions.IsAdminOrIfAuthenticatedReadOnly",
+    ]
 }
 
 SPECTACULAR_SETTINGS = {
