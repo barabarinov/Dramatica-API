05fac1b02c9bf58c186ca2869e79e264c3cf97b6
barabarinov <barabarinov@gmail.com>
2024-03-28 17:44:04 +0200

    fixed play_image_file_path function

--- theatre/models.py
+++ theatre/models.py
@@ -52,7 +52,7 @@ def play_image_file_path(instance: "Play", filename: str) -> str:
     """Generate file path for uploading play images."""
     _, extension = os.path.splitext(filename)
     unique_filename = (
-        f"{slugify(instance.title)}-{instance.id}-{uuid.uuid4()}{extension}"
+        f"{slugify(instance.title)}-{uuid.uuid4()}{extension}"
     )
 
     return os.path.join("uploads", "plays", unique_filename)
